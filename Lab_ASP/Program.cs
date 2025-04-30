f0e9e225a3213d4347de203ff4360fa7e681cd54
s0s3nk4 <msosna2929@gmail.com>
2025-04-30 15:40:11 +0200

    dodanie layoutu sb admin

--- Lab_ASP/Program.cs
+++ Lab_ASP/Program.cs
@@ -60,12 +60,12 @@ public class Program
 
         // Configure routes
         app.MapControllerRoute(
-            name: "default",
-            pattern: "{controller=Home}/{action=Index}/{id?}");
-
-        app.MapControllerRoute(
             name: "areas",
             pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+        app.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}");
         app.MapRazorPages();
 
         // Ensure database is created
